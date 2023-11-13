9fb053e740638fb65b75aeb68d2cf28a9f17fa8c
Weiwei <linweiwei2222@163.com>
2023-11-13 18:32:02 -0500

    practice

---
+++ travel_list/src/components/Item.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Item({ item, onDeleteItem, onToggleItem }) {
+  return (
+    <li>
+      <input type="checkbox" onChange={() => onToggleItem(item.id)} />
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity}
+        {item.description}
+      </span>
+      <button onClick={() => onDeleteItem(item.id)}>&times;</button>
+    </li>
+  );
+}
+
+export default Item;
